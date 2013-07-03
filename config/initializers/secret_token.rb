cabd98739ea80d3336ec44dc705d5c79783ae282
lulalala <mark@goodlife.tw>
2013-07-03 10:05:09 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cellmail::Application.config.secret_token = '5fd71e967b9fdf3fd7d504b7e1e15d4f094c40efca50c723440bbcbee53493f561f713ebd8dbdc4a356d2d3d0710e2258108ff32698006247a7251086a409f2c'
